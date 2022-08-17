6f1d24ed593954ac552a4877612b4d699fd34f44
Palmby <kaleb_palmby@yahoo.com>
2022-08-16 23:24:11 -0400

    Compare File changes
    Upload CSV files from both areas and compare

---
+++ Compare two CSV for differences.ps1
@@ -0,0 +1,4 @@
+﻿$adcomp = import-csv 'C:\users\Kaleb Palmby\Downloads\computerlist.csv'
+$ncentral = import-csv 'C:\users\Kaleb Palmby\Downloads\lan.csv'
+compare-object $adcomp $ncentral -Property name -IncludeEqual | export-csv C:\posh\comparative.csv
+
