5c677eb34ecf55f673a677c36889f9d5df7cbecc
Palmby <kaleb_palmby@yahoo.com>
2023-01-19 13:49:38 -0500

    Update Compare two CSV for differences.ps1

--- Compare two CSV for differences.ps1
+++ Compare two CSV for differences.ps1
@@ -1,4 +1,17 @@
-﻿$adcomp = import-csv 'C:\users\Kaleb Palmby\Downloads\computerlist.csv'
-$ncentral = import-csv 'C:\users\Kaleb Palmby\Downloads\lan.csv'
-compare-object $adcomp $ncentral -Property name -IncludeEqual | export-csv C:\posh\comparative.csv
+$CSV1 = $null
+$CSV2 = $null
 
+$CSV1 = Import-Csv -Path '' 
+$CSV2 = Import-Csv -Path ''
+
+$dif = Compare-Object -ReferenceObject $CSV1 -DifferenceObject $CSV2 -Property <propertyname>
+
+foreach($y in $dif){
+   if($y.SideIndicator -eq "=>"){
+    write-output $y.name "Is present in the difference but not in reference."
+   }
+   if($y.SideIndicator -eq "<="){
+    write-output $y.Name "Is present in reference but not in difference"
+   }
+
+}
