ba3aa822a8c7c165dd15587ed1a63f91338697b2
cranky_sysadmin <kaleb_palmby@yahoo.com>
2023-06-06 11:44:25 -0400

    DUO Custom API - Get user and logon List
    Get users and their last logon to DUO authentication.. Note, must have custom API setup in Powershell Module to work

---
+++ DUO Custom API - Get user list and logon days.ps1
@@ -0,0 +1,33 @@
+﻿#note: Must have the custom DUO module installed on machine to run
+#instructions here: https://github.com/mbegan/Duo-PSModule#readme
+
+import-module Duo
+
+$users = duogetuser 
+
+
+foreach ($user in $users)
+{
+   $u = $user.realname
+   $username = $user.username
+   
+   $unixtime = $user.last_login
+   
+   $date = (get-date 01.01.1970)+([System.TimeSpan]::fromseconds($unixtime))
+   
+  
+
+   $prop = @{
+
+   "name" = $u 
+   "username" = $username
+   "Date" = $date
+
+   }
+
+   $obj = new-object PSObject -property $prop
+
+   $obj | select 'name', 'username', 'Date' | sort name|  export-csv C:\temp\duolist.csv -NoTypeInformation -Append
+
+        
+}
