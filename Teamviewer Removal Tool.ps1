013ae8888af68e9852b2f591cd296f6eedfcde52
cranky_sysadmin <kaleb_palmby@yahoo.com>
2023-06-06 15:51:22 -0400

    Teamviewer Silent Removal Tool
    Silently removes Teamviewer

---
+++ Teamviewer Removal Tool.ps1
@@ -0,0 +1,29 @@
+﻿#Stop Teamviewer
+net stop Teamviewer
+
+# check what folder teamviewer is in 
+
+$x64 = Test-Path 'C:\Program Files\Teamviewer' 
+
+$x86 = Test-path 'C:\Program Files (x86)\Teamviewer'
+
+
+if ("$x64" -eq $true)
+{
+    cd "C:\Program Files\Teamviewer" 
+    .\uninstall.exe /S 
+    Write-output "Teamviewer Uninstalled under the Program Files Folder"
+}
+
+elseif("$x86" -eq $true)
+
+{
+    cd "C:\Program Files (x86)\Teamviewer"
+    .\uninstall.exe /S 
+    Write-output "Teamviewer Uninstalled under the Program Files (x86) Folder"
+}
+
+else 
+{
+    write-output "Teamviewer not on machine"
+}
